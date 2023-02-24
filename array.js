dcfa3b865ea2e9f487cbff38b1391ac7f5ba51b5
Vaibhav-Kishor <vaibhavkishor7@gmail.com>
2023-02-25 02:03:54 +0530

    2nd day

---
+++ array.js
@@ -0,0 +1,44 @@
+//! Array
+//* ordered collection of items
+
+// let fruits = ["apple","mango", "grapes"];
+// console.log(fruits);
+
+// let numbers = [1,2,3,4,5];
+// console.log(numbers);
+
+// let mixed = [1,2,3,"string",null,undefined];
+// console.log(mixed);
+
+// let fruits = ["apple","mango", "grapes"];
+// fruits[0]="banana";
+// console.log(fruits);
+// console.log(typeof(fruits));//! output: object
+
+//! Methods
+//? isArray
+// let fruits = ["apple","mango", "grapes"];
+// console.log(Array.isArray(fruits));
+
+//? Push & Pop
+// let fruits = ["apple","mango", "grapes"];
+// console.log(fruits);
+// fruits.push("Guava");
+// console.log(fruits);
+
+// fruits.pop(); //! remove & return also
+// console.log(fruits);
+// let removeFruits = fruits.pop();
+// console.log(removeFruits);
+
+//? unshift
+// let fruits = ["apple","mango", "grapes"];
+// fruits.unshift("strawberry");//! add at fruits[0]
+// console.log(fruits);
+
+//? shift
+let fruits = ["apple","mango", "grapes"];
+// fruits.shift();//! remove from last & return
+
+
+
---
+++ trutyFalsyValue_03.js
@@ -0,0 +1,57 @@
+//! Truthy and Falsy Value
+//? 1. Truthy Value
+// any data except falsy
+//? 2. Falsy Value
+//* false
+//* ""
+//* null
+//* undefined
+//* 0
+
+// ex-
+let firstName = false;
+if(firstName){
+    console.log(firstName);
+}
+else{
+    console.log("FirstName is empty");
+}
+
+
+firstName = null;
+if(firstName){
+    console.log(firstName);
+}
+else{
+    console.log("FirstName is empty");
+}
+
+firstName = "";
+if(firstName){
+    console.log(firstName);
+}
+else{
+    console.log("FirstName is empty");
+}
+
+firstName = undefined;
+if(firstName){
+    console.log(firstName);
+}
+else{
+    console.log("FirstName is empty");
+}
+
+firstName = 0;
+if(firstName){
+    console.log(firstName);
+}
+else{
+    console.log("FirstName is empty");
+}
+
+
+
+
+
+
